f807fd089b52599a27dd7c8f9a87bec4440024b3
dlyyds <1209304192@qq.com>
2026-07-27 17:53:05 +0800

    feat(scene): 实现 Scene 类基础框架（entt::registry + Entity 管理）
    - 基于 entt::registry 实现实体管理
    - CreateEntity / DestroyEntity 接口
    - 自动添加 TagComponent 和 TransformComponent
    - OnUpdate / OnViewportResize 占位实现
    - OnComponentAdded 模板特化机制（Transform/Tag/SpriteRenderer）

--- GE/include/GE/Scene/Scene.h
+++ GE/include/GE/Scene/Scene.h
@@ -1,8 +1,40 @@
-//
-// Created by Lenovo on 2026/7/27.
-//
+#pragma once
 
-#ifndef GE_SCENE_H
-#define GE_SCENE_H
+#include "entt.hpp"
 
-#endif //GE_SCENE_H
+
+#include "Core/Timestep.h"
+
+namespace GE {
+
+class Entity;
+
+class Scene {
+public:
+    Scene();
+
+    ~Scene();
+
+    Entity CreateEntity(const std::string &name = "Entity");
+
+    void DestroyEntity(Entity entity);
+
+    // TEMP
+    entt::registry &Reg() { return m_Registry; }
+
+    void OnUpdate(Timestep ts);
+
+    void OnViewportResize(uint32_t width, uint32_t height);
+
+private:
+    entt::registry m_Registry;
+    uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+
+    template <typename T>
+    void OnComponentAdded(Entity entity, T &component);
+
+    friend class Entity;
+
+};
+
+}
--- GE/src/Scene/Scene.cpp
+++ GE/src/Scene/Scene.cpp
@@ -1,3 +1,61 @@
-//
-// Created by Lenovo on 2026/7/27.
+#include "pch.h"
+#include "Scene/Scene.h"
+#include "Scene/Components.h"
+#include "Scene/Entity.h"
+
+
+#include <glm/glm.hpp>
+
+namespace GE {
+
+
+Scene::Scene() = default;
+
+Scene::~Scene() = default;
+
+Entity Scene::CreateEntity(const std::string &name) {
+    Entity entity{m_Registry.create(), this};
+
+    entity.AddComponent<TransformComponent>();
+    entity.AddComponent<TagComponent>(name);
+
+    return entity;
+}
+
+void Scene::DestroyEntity(Entity entity) {
+    m_Registry.destroy(static_cast<entt::entity>(entity));
+}
+
+
+void Scene::OnUpdate(Timestep ts) {
+
+}
+
+
+void Scene::OnViewportResize(const uint32_t width, const uint32_t height) {
+
+}
+
+
+template <typename T>
+void Scene::OnComponentAdded(Entity entity, T &component) {
+    static_assert(false);
+}
+
+template <>
+void Scene::OnComponentAdded<TransformComponent>(Entity entity, TransformComponent &component) {
+}
+
+
+template <>
+void Scene::OnComponentAdded<TagComponent>(Entity entity, TagComponent &component) {
+}
+
+template <>
+void Scene::OnComponentAdded<SpriteRendererComponent>(Entity entity, SpriteRendererComponent &component) {
+}
+
+
+} //
+// Created by Lenovo on 2026/5/9.
 //
